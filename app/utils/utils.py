3a9b28f1aba0466d42b3a5addfd940984eaee3d8
shubhamm2712 <shubham.maheshwari2@gmail.com>
2024-08-22 16:42:24 -0700

    Changed cust_brok to customer in utils

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -1,20 +1,20 @@
-from typing import Union, List, Dict
+from typing import List, Dict
 
 from ..config.consts import ORG, ORG_NOT_FOUND
 from ..models.models import Customer
 from ..exceptions import UnauthorizedException
 
-def set_org_model(cust_brok: Customer, auth_result: Dict) -> Customer:
+def set_org_model(customer: Customer, auth_result: Dict) -> Customer:
     if ORG in auth_result:
-        cust_brok.org = auth_result[ORG]
+        customer.org = auth_result[ORG]
     else:
         raise UnauthorizedException(ORG_NOT_FOUND)
-    return cust_brok
+    return customer
 
-def set_org_multiple_model(cust_broks: List[Customer], auth_result: Dict) -> List[Customer]:
+def set_org_multiple_model(customers: List[Customer], auth_result: Dict) -> List[Customer]:
     if ORG in auth_result:
-        for cust_brok in cust_broks:
-            cust_brok.org = auth_result[ORG]
+        for customer in customers:
+            customer.org = auth_result[ORG]
     else:
         raise UnauthorizedException(ORG_NOT_FOUND)
-    return cust_broks
+    return customers
